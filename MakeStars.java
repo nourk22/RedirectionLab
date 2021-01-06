c99160a40f490d48dc10605b8a6da71cb18bd1c9
Nour <>
2021-01-05 19:07:02 -0500

    it works

--- MakeStars.java
+++ MakeStars.java
@@ -1,19 +1,28 @@
 import java.util.*;
 
 public class MakeStars{
-  public static String Stars(String word){
+  public static String Word(String word){
     String stars = "";
     for (int i=0; i<word.length(); i++){
       stars+="*";
     }
+    stars+=" ";
     return stars;
   }
 
   public static void main(String[] args){
-      Scanner n = new Scanner(System.in);
-      while (n.hasNext()){
-        String curr = n.next();
-        System.out.print(Stars(curr));
+
+    Scanner n = new Scanner(System.in);
+    while (n.hasNextLine()){
+      String curr_line = n.nextLine();
+      Scanner test = new Scanner(curr_line);
+
+
+      while (test.hasNext()){
+        String word = test.next();
+        System.out.print(Word(word));
       }
+      System.out.println();
+    }
   }
 }
