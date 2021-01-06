9b49dff887e0cb36ae8414bce31d89233d35a4ee
Nour <>
2021-01-05 19:28:32 -0500

    step 2 compiles

--- PigLatin.java
+++ PigLatin.java
@@ -11,6 +11,17 @@ public class PigLatin{
     return ans;
   }
 
+  public static String pigLatin(String s){
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+    for (int i = 0; i < digraphs.length; i++){
+      if (s.substring(0, 2).equals(digraphs[i])){
+        return s.substring(2) + digraphs[i] + "ay";
+      }
+    }
+    return pigLatinSimple(s);
+  }
+
   public static void main(String[] args){
 
     Scanner n = new Scanner(System.in);
